f45187c5904f71f1dd92509926d1adb2eeaab4f9
attila5287 <atiturkoz@hotmail.com>
2021-03-11 05:51:58 -0700

    add qst

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var express = require("express");
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 5000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
