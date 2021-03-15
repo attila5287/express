0e3f37b3c2b7ce577b6d23e62c5f3baa10c7bfc2
attila5287 <atiturkoz@hotmail.com>
2021-03-15 01:34:22 -0600

    api/notes returns json

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
 
 // Sets up the Express App
 const app = express();
-// const PORT = 3000;
+// const PORT = 5000;
 let PORT = process.env.PORT || 5000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-// Basic route that sends the user first to the AJAX Page
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
-
+require("./routes/api")(app);
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
---
+++ routes/api.js
@@ -0,0 +1,38 @@
+module.exports = function (app) {
+const fs = require('fs');
+app.get("/", (req, res) =>
+	res.sendFile(path.join(__dirname, "./public/index.html"))
+);
+app.get("/notes", (req, res) =>
+	res.sendFile(path.join(__dirname, "./public/notes.html"))
+);
+
+app.get("/api/notes", function (req, res) {
+	fs.readFile("./db/db.json", function (err, data) {
+		if (err) {
+			console.log("err :>> ", err);
+		}
+		console.log("JSON.parse(data)");
+		console.log(JSON.parse(data));
+		res.json(JSON.parse(data));
+	});
+});
+
+	// API GET Requests
+	// Below code handles when users "visit" a page.
+	// In each of the below cases when a user visits a link
+	// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+	// ---------------------------------------------------------------------------
+
+	// ---------------------------------------------------------------------------
+	// I added this below code so you could clear out the table while working with the functionality.
+	// Don"t worry about it!
+
+	app.post("/api/clear", function (req, res) {
+		// Empty out the arrays of data
+		tableData.length = 0;
+		waitListData.length = 0;
+
+		res.json({ ok: true });
+	});
+};
