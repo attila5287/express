a78285c46b6995b6697e6645af99d887de5ef26c
attila5287 <atiturkoz@hotmail.com>
2021-03-17 07:31:02 -0600

    db path issue still

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 
 // Sets up the Express App
-const app = express();
-// const PORT = 5000;
-let PORT = process.env.PORT || 5000;
+const app = express()
+// const PORT = 5000
+let PORT = process.env.PORT || 5000
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+// export as function
+require("./routes/html")(app)
+require("./routes/api")(app)
 
-require("./routes/html")(app);
-require("./routes/api")(app);
 
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`))
+
--- routes/api.js
+++ routes/api.js
@@ -1,33 +1,29 @@
 module.exports = function (app) {
+// app.use(express.static("public"));
 const fs = require("fs");
 const path = require("path");
-
 app.get("/api/notes", function (req, res) {
 	fs.readFile("./db/db.json", function (err, data) {
 		if (err) {
 			console.log("err :>> ", err);
 		}
-		console.log("JSON.parse(data)");
-		console.log(JSON.parse(data));
 		res.json(JSON.parse(data));
 	});
 });
-
-	// API GET Requests
-	// Below code handles when users "visit" a page.
-	// In each of the below cases when a user visits a link
-	// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-	// ---------------------------------------------------------------------------
-
-	// ---------------------------------------------------------------------------
-	// I added this below code so you could clear out the table while working with the functionality.
-	// Don"t worry about it!
-
-	app.post("/api/clear", function (req, res) {
-		// Empty out the arrays of data
-		tableData.length = 0;
-		waitListData.length = 0;
-
-		res.json({ ok: true });
+// POST
+app.post("/api/notes", function (req, res) {
+	console.log('req');
+	console.log(req);
+	console.log('---');
+	fs.readFile("./db/db.json", function (err, data) {
+	fs.writeFile("./db/db.json", res.json(JSON.parse(data).push(req.body)), () => { console.log('success write');
+		
+	})
 	});
+});
+
+
+// ---
+// dtt
+// ---
 };
